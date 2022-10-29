b6c4d1fe2b8b0e80c1071b48d28a75ce3b9b70c0
Kelvin Sousa <sousa.kelvin@outlook.com>
2022-10-28 22:39:49 -0300

    adjust-code

--- mad-libs/app.py
+++ mad-libs/app.py
@@ -1,11 +1,4 @@
 
-
-# verb1 = input(str('Verb 1: ')).strip()
-# verb2 = input(str('Verb 2: ')).strip()
-# famous_person = input(str('Famous Person: ')).strip()
-
-
-
 def madlib(adj, verb1, verb2, famous_person):
     print(f'computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. \nStay hydrated and {verb2} like you are {famous_person}!')
 
@@ -16,7 +9,6 @@ def Check(input_list):
             print(f'O valor {items} digitado é inválido!')
             break
     else:
-        print('Valores válidos')
         madlib(adj, verb1, verb2, famous_person)
 
 
